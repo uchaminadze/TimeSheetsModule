51af16e5cd4e0e2cbf59c21dc0befeaddf817cef
uchaminadze <uchasmailbox@gmail.com>
2023-05-30 19:59:36 +0400

    little fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 function App() {
-  const [data, setData] = useState('');
-
-  useEffect(() => {
-    (async function () {
-      const { text } = await( await fetch(`/api/message`)).json();
-      setData(text);
-    })();
-  });
-
-  return <div>{data}</div>;
+  return <div>Hello from react app</div>;
 }
 
 export default App;
